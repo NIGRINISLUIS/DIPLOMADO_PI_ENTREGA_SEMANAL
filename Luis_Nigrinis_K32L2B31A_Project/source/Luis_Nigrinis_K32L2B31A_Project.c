56d95614019c28d949564f91dbf57cf4f9517b2f
luis_nigrinis <nigrinisluis@gmail.com>
2021-09-12 13:02:06 -0500

    Cofiguracion del codigo principal, boton1=sensor_luz, botton2= sensor_temperatura, led rojo y verde frecuencia 1hz

--- Luis_Nigrinis_K32L2B31A_Project/source/Luis_Nigrinis_K32L2B31A_Project.c
+++ Luis_Nigrinis_K32L2B31A_Project/source/Luis_Nigrinis_K32L2B31A_Project.c
@@ -18,48 +18,16 @@
 #include "clock_config.h"
 #include "K32L2B31A.h"
 #include "fsl_debug_console.h"
-
 #include "leds.h"
 #include "sensor_de_luz.h"
 #include "irq_lptmr0.h"
 #include "botones.h"
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
+#include "sensor_temperatura.h"
 
-/*******************************************************************************
- * Private Prototypes
- ******************************************************************************/
-
-/*******************************************************************************
- * External vars
- ******************************************************************************/
-
-/*******************************************************************************
- * Local vars
- ******************************************************************************/
-unsigned int test_global_var=100;
-float dato_float=3.145;
-
-/*******************************************************************************
- * Private Source Code
- ******************************************************************************/
-
-/*
- * @brief generar bloqueo a microcontrolador por tiempo fijo
- */
-
-
-/*
- * @brief interrupcion por LPTMRO cada un segundo
- */
 
 
 
 int main(void) {
-
-	uint32_t adc_sensor_de_luz;
-	bool boton1,boton2;
 
     /* Init board hardware. */
     BOARD_InitBootPins();
@@ -70,45 +38,67 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\r\n");
-    PRINTF("test_global_var: %d\r\n",test_global_var);
-    PRINTF("dato_float: %g\r\n",dato_float);
-    led_on_green();
+    //PRINTF("Hello World\r\n");
+   // PRINTF("test_global_var: %d\r\n",test_global_var);
+    //PRINTF("dato_float: %g\r\n",dato_float);
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    //unsigned char cont_LR = 0;
-    /* Enter an infinite loop, just incrementing a counter. */
-
-    /* Start counting */
     LPTMR_StartTimer(LPTMR0);
 
+    /*trabajo para entregar*/
+     /*
+      * 1. leer sensor de temperatura (puede ser el interno o el lm35) y hacer un printf(temperatura)
+      * 2. medida de sensor de luz en unidades de lux
+      * 3. la medida de temperatura y lux se imprina  temperatura solo cuando se pulsa el boton 1 y lux con el boton 2(debe imprimir en tiempo real)
+      */
+
+    /* Force the counter to be placed into memory. */
+//   volatile static int i = 0 ;
+    bool boton1,boton2;
+   float adc_sensor_de_luz;
+    float adc_sensor_de_temperatura;
+    /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
 
-        if (lptmr0_irq_counter==1){
-        	led_off_red();
-        	led_on_green();
-          	i++ ;
+    if (lptmr0_irq_counter !=0){
 
+    			if (lptmr0_irq_counter==10){
+    	            	led_off_red();
+    	            	led_on_green();
+
+    	        }
+    	        if (lptmr0_irq_counter==20){
+    	        		led_on_red();
+    	            	led_off_green();
+    	            	lptmr0_irq_counter=0;
+
+    	        }
+
+       //i++ ;
+        //printf("i:%u\r\n",i);
+    	boton1=boton1leerestado();
+    	boton2=boton2leerestado();
+    	if (boton1 && !boton1_presionado){
+    		boton1_presionado=1;
+    		adc_sensor_de_luz=sensorDeLuzObtenerDatosADC();
+    		printf("sensor de luz en unidades  lux :%.4f\r\n",adc_sensor_de_luz);
+    	}
+
+    	if (!boton1){
+    		boton1_presionado=0;
+    	}
+    	if (boton2 && !boton2_presionado){
+    	    boton2_presionado=1;
+    	    adc_sensor_de_temperatura=SensorDetemperaturaObtenerDatosADC();
+    	    printf("Temperatura en grados centigrados  :%.4f\r\n",adc_sensor_de_temperatura);
+    	}
+
+    	if (!boton2){
+
+    	    boton2_presionado=0;
         }
-        if (lptmr0_irq_counter==2){
-        	led_on_red();
-        	led_off_green();
-        	lptmr0_irq_counter=0;
-         	i++ ;
-         	boton1=boton1leerestado();
-         	boton2=boton2leerestado();
-
-         	printf("Boton1 = %u\r\n",boton1);
-         	printf("Boton2 = %u\r\n",boton2);
-
-          	adc_sensor_de_luz=sensordeluzobtenerdatoADC();
-          	printf("ADC sensor de luz %u\r\n",adc_sensor_de_luz);
-
-        }
-
+    }
 
 
     }
-    return 0 ;
+   return 0 ;
 }
