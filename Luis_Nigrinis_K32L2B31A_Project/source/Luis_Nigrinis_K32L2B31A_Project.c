e1b79e772373c794814cd5d37682dd4646503f14
luis_nigrinis <nigrinisluis@gmail.com>
2021-09-15 20:31:14 -0500

    Modificacion codigo, asignacion de comandos AT, lectura de los leds Rojo y Verde, lectura sensor de luz y temperatura (Docklight)

--- Luis_Nigrinis_K32L2B31A_Project/source/Luis_Nigrinis_K32L2B31A_Project.c
+++ Luis_Nigrinis_K32L2B31A_Project/source/Luis_Nigrinis_K32L2B31A_Project.c
@@ -27,52 +27,123 @@
 #include "irq_lptmr0.h"
 #include "irq_lpuart0.h"
 
-#define MENSAJE1_DEFINE "AT+CPIN\r\n"
-#define COMANDOS_DISPONIBLES 2
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
 #define SIZE_BUFFER_COMANDOS  50
 
+#define COMANDOS_DISPONIBLES 6
+#define COMANDOS_DISPONIBLES_EC25 24
+#define COMANDOS_RESPUESTA_OK 1
 
-char mensaje_ram[]="AT+CPIN\r\n";
-const char mensaje_flash[]="AT+CPIN\r\n";
-char mensaje1[]="AT+CPIN\r\n";
 
-enum _ec25_lista_comandos_at {
-	kAT= 0,
-	kATI,
-	kAT_CPIN,
-	kAT_CREG,
-	kAT_CMGF_1,
-	kAT_CMGS,
-	kAT_TEXT_MSG_END,
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+
+uint32_t index_buffer_nuevo_comando_recibido=0;
+uint8_t nuevo_byte_lpuart0;
+char buffer_nuevo_comando_recibido[50];
+uint8_t i=0;
+
+/******************************/
+enum _lista_comandos_controll{
+	GREEN_ON = 0,
+	GREEN_OFF,
+	RED_ON,
+	RED_OFF,
+	SENSOR_TEMPERATURA,
+	SENSOR_LUZ,
+
 };
 
-enum _lista_comandos_controll{
-	led_verde_on = 0,
-	led_verde_off,
+const char *comandos_control[] ={
+		"GREEN_ON",  //encender led VERDE
+		"GREEN_OFF",  //apagar led VERDE
+		"RED_ON",     //encender led ROJO
+		"RED_OFF",    //apagar led ROJO
+		"SENSOR_TEMPERATURA",    //Imprimir datos del sensor de temperatura
+		"SENSOR_LUZ",    //No imprime datos del sensor
+
+};
+
+/*******************************/
+enum lista_ec25_comandos_at{
+	informacion_del_producto=0,
+	identificacion_del_producto,
+	identificacion_del_modelo_de_TA,
+	identificacion_de_revision_versiondesoftware,
+	Solicitar_identificacion_del_fabricante,
+	Solicitar_identificacion_de_modelo,
+	Solicitar_TA_de_la_version_d_esoftware,
+	Solicitar_identidad_del_equipo_movil_IMEI,
+	Identificacion_numero_de_serie_del_producto,
+	Establecer_valores_predeter_del_fabricante,
+	Mostrar_configuracion_actual,
+	Almacenar_parametros_actuales,
+	Establecer_todos_los_parametros_actuales,
+	Establecer_el_modo_de_presentacion,
+	Establecer_modo_de_eco_de_comando,
+	Repetir_linea_de_comando_anterior,
+	Establecer_determinacion_de_la_linea_de_comandos,
+	Establecer_caracter_de_formato_de_respuesta,
+	Establecer_caracter_de_edicion_delineade_comando,
+	Resultado_de_CONNECT_monitore0_progreso_llamada,
+	Configurar_la_funcionalidad_del_telefono,
+	Formato_del_mensaje_de_error,
+	Seleccione_el_juego_de_caracteres_TE,
+	Configurar_la_opcion_de_indicacion_URC,
 
 };
 
 
 const char *ec25_comandos_at[]={
-		"AT",  //COMPRUEBA DISPONIBILIDAD DEL SIPOSITIVO
-		"ATI",  //CONSULTA INFORMACION EL MODEN
-		"AT+CPIN?",   //CONSULTA ESTADO DE LA SIMCARD
-		"AT+CREG?",   //CONSULTA ESTADO DE LA RED CELULAR Y TECNOLOGIA Y TECNOLOGIA USADA EN RED CELULAR
-		"AT+CMGF-1",  //SIGNA MODO TEXTO PARA ENVIAR MENSAJES
-		"AT+CMGS=\"300XXXXXX\"", //ENVIA MENSAJES DE TEXTO A NUMERO DEFINIDO
-		"MENSAJE", //MENSAJE & CTRL+Z
+		"informacion_del_producto",  //CONSULTA INFORMACION EL MODEN
+		"identificacion_del_producto",
+		"identificacion_del_modelo_de_TA",
+		"identificacion_de_revision_versiondesoftware",
+		"Solicitar_identificacion_del_fabricante",
+		"Solicitar_identificacion_de_modelo",
+		"Solicitar_TA_de_la_version_d_esoftware",
+		"Solicitar_identidad_del_equipo_movil_IMEI",
+		"Identificacion_numero_de_serie_del_producto",
+		"Establecer_valores_predeter_del_fabricante",
+		"Mostrar_configuracion_actual",
+		"Almacenar_parametros_actuales",
+		"Establecer_todos_los_parametros_actuales",
+		"Establecer_el_modo_de_presentacion",
+
+		"Establecer_modo_de_eco_de_comando",
+		"Repetir_linea_de_comando_anterior",
+		"Establecer_determinacion_de_la_linea_de_comandos",
+		"Establecer_caracter_de_formato_de_respuesta",
+		"Establecer_caracter_de_edicion_delineade_comando",
+		"Resultado_de_CONNECT_monitore0_progreso_llamada",
+		"Configurar_la_funcionalidad_del_telefono",
+		"Formato_del_mensaje_de_error",
+		"Seleccione_el_juego_de_caracteres_TE",
+		"Configurar_la_opcion_de_indicacion_URC",
+
+
 };
 
-const char *comandos_control[] ={
-		"led_verde_on",  //encender led rojo
-		"led_verde_off",  //apagar led rojo
+/************************************/
+enum lista_respuesta_ec25_OK{
+	OK=0,
 
 };
 
-char buffer_nuevo_comando_recibido[50];
-uint32_t index_buffer_nuevo_comando_recibido=0;
 
-uint8_t i=0;
+const char *respuesta_ec25_OK[]={
+		"OK",
+
+
+};
+
+/************************************/
 void LIMPIAR_BUFFER(){
 	for(i=0;i<50;i++){
 		buffer_nuevo_comando_recibido[i]=0x00;
@@ -80,135 +151,231 @@ void LIMPIAR_BUFFER(){
 	index_buffer_nuevo_comando_recibido=0;
 }
 
+/*******************************************************************************
+ * Public Source Code
+ ******************************************************************************/
+
+
+
 int main(void) {
 
 
-	uint8_t nuevo_byte_lpuart0;
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-#endif
+    /* Init board hardware. *//***********************/
+    BOARD_InitBootPins();     /***********************/
+    BOARD_InitBootClocks();   /***********************/
+    BOARD_InitBootPeripherals();/*********************/
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL/**********/
+    /* Init FSL debug console. */ /*******************/
+    BOARD_InitDebugConsole();   /*********************/
+#endif                         /**********************/
 
 
+    printf("FRDM-K32L2B3 iniciando......\r\n");
 
-    PRINTF(" FRDM-K32L2B3 iniciando...\r\n");
-
-
-    printf("flash--- %s \r\n",mensaje_flash);
-    printf("ram----%s \r\n",mensaje_ram);
-    printf("define---%s \r\n",MENSAJE1_DEFINE);
-
-    printf("define---%s \r\n",MENSAJE1_DEFINE);
-
-    for(int i=0; i<=6 ; i++){
-
-    	printf("%s \r\n",ec25_comandos_at[i]);
-    }
-
-
-
-
-
-    LPTMR_StartTimer(LPTMR0);
-
-
-
-
+    LPTMR_StartTimer(LPTMR0); //Inicializar LPTMR0
     bool boton1,boton2;
-    float adc_sensor_de_luz;
-    float adc_sensor_de_temperatura;
-    /* Enter an infinite loop, just incrementing a counter. */
+   float adc_sensor_de_luz;
+   float adc_sensor_de_temperatura;
 
 
-
+   /************************************************************************************************/
     while(1) {
 
 
-
     	if(NumeroDeByteDisponiblesEnBufferRx()!=0){
-    		nuevo_byte_lpuart0=pullBytedesdeBuefferCircular();
 
+    		nuevo_byte_lpuart0=pullBytedesdeBuefferCircular();
     		buffer_nuevo_comando_recibido[index_buffer_nuevo_comando_recibido]=nuevo_byte_lpuart0;
     		index_buffer_nuevo_comando_recibido++;
 
     		char *puntero_ok;
-
-
     		if(nuevo_byte_lpuart0=='\n'){
-
     			for(int i=0;i<COMANDOS_DISPONIBLES;i++){
-
     				puntero_ok=(char*)(strstr((char*)(&buffer_nuevo_comando_recibido[0]),(char*)(comandos_control[i])));
     				if(puntero_ok!=0){
     					switch(i){
-    					case led_verde_on:
+
+    					case GREEN_ON:
     					led_on_green();
     					break;
 
-    					case led_verde_off:
+    					case GREEN_OFF:
     					led_off_green();
     				    break;
+
+    					case RED_ON:
+    					led_on_red();
+    					break;
+
+    					case RED_OFF:
+    					led_off_red();
+    					break;
+
+    					case SENSOR_TEMPERATURA:
+    					adc_sensor_de_temperatura=SensorDetemperaturaObtenerDatosADC();
+    					printf("Temperatura en grados centigrados  : %.4f\r\n",adc_sensor_de_temperatura);
+    					break;
+
+    					case SENSOR_LUZ:
+    					adc_sensor_de_luz=sensorDeLuzObtenerDatosADC();
+    					printf("sensor de luz en unidades  lux : %.4f\r\n",adc_sensor_de_luz);
+    					break;
+
     					}
     				}
     			}
 
-    			LIMPIAR_BUFFER();
 
+      			for(int i=0;i<COMANDOS_DISPONIBLES_EC25;i++){
+        			puntero_ok=(char*)(strstr((char*)(&buffer_nuevo_comando_recibido[0]),(char*)(ec25_comandos_at[i])));
+        			if(puntero_ok!=0){
+        				switch(i){
+
+        				case informacion_del_producto:
+        				printf("ATI\r\n");
+        				break;
+
+        				case identificacion_del_producto:
+        				printf("AT+GMI\r\n");
+        				break;
+
+        				case identificacion_del_modelo_de_TA:
+        				printf("AT+GMM\r\n");
+        				break;
+
+        				case identificacion_de_revision_versiondesoftware:
+        				printf("AT+GMR\r\n");
+        				break;
+
+        				case Solicitar_identificacion_del_fabricante:
+        				printf("AT+CGMI\r\n");
+        				break;
+
+        				case Solicitar_identificacion_de_modelo:
+        				printf("AT+CGMM\r\n");
+        				break;
+
+        				case Solicitar_TA_de_la_version_d_esoftware:
+        				printf("AT+CGMR\r\n");
+        				break;
+
+        				case Solicitar_identidad_del_equipo_movil_IMEI:
+        				printf("AT+GSN\r\n");
+        				break;
+
+        				case Identificacion_numero_de_serie_del_producto:
+        				printf("AT+CGSN\r\n");
+        				break;
+
+        				case Establecer_valores_predeter_del_fabricante:
+        				printf("AT&F\r\n");
+        				break;
+
+        				case Mostrar_configuracion_actual:
+        				printf("AT&V\r\n");
+        				break;
+
+        				case Almacenar_parametros_actuales:
+        				printf("AT&W\r\n");
+        				break;
+
+        				case Establecer_todos_los_parametros_actuales:
+        				printf("ATZ\r\n");
+        				break;
+
+        				case Establecer_el_modo_de_presentacion:
+        				printf("ATQ\r\n");
+        				break;
+
+        				case Establecer_modo_de_eco_de_comando:
+        				printf("ATE\r\n");
+        				break;
+
+        				case Repetir_linea_de_comando_anterior:
+        				printf("A/\r\n");
+        				break;
+
+        				case Establecer_determinacion_de_la_linea_de_comandos:
+        				printf("ATS3\r\n");
+        				break;
+
+        				case Establecer_caracter_de_formato_de_respuesta:
+        				printf("ATS4\r\n");
+        				break;
+
+        				case Establecer_caracter_de_edicion_delineade_comando:
+        				printf("ATS5\r\n");
+        				break;
+
+        				case Resultado_de_CONNECT_monitore0_progreso_llamada:
+        				printf("ATX\r\n");
+        				break;
+
+        				case Configurar_la_funcionalidad_del_telefono:
+        				printf("AT+CFUN\r\n");
+        				break;
+
+        				case Formato_del_mensaje_de_error:
+        				printf("AT+CMEE\r\n");
+        				break;
+
+        				case Seleccione_el_juego_de_caracteres_TE:
+        				printf("AT+CSCS\r\n");
+        				break;
+
+        				case Configurar_la_opcion_de_indicacion_URC:
+        				printf("AT+QURCCFG\r\n");
+        				break;
+        				}
+        			}
+        		}
+
+      			for(int i=0;i<COMANDOS_RESPUESTA_OK;i++){
+      			     puntero_ok=(char*)(strstr((char*)(&buffer_nuevo_comando_recibido[0]),(char*)(respuesta_ec25_OK[i])));
+      			     if(puntero_ok!=0){
+      			        switch(i){
+
+      			        case OK:
+      					printf("   COMANDO  OK.\r\n");
+      					break;
+
+
+      			        }
+      			      }
+      			}
+
+      			LIMPIAR_BUFFER();
     		}
-    		//printf("dato: %c, 0x%x\r\n\r\n", nuevo_byte_lpuart0);
-    	}
-    if (lptmr0_irq_counter !=0){
-
-    			if (lptmr0_irq_counter==10){
-    				    led_on_red();
+    	} // fin del if  NumeroDeByteDisponiblesEnBufferRx
 
 
-    	        }
-    			if (lptmr0_irq_counter==19){
-    				printf("Temperatura en grados centigrados  :%.4f\r\n",adc_sensor_de_temperatura);
-    			}
-    	        if (lptmr0_irq_counter==20){
-    	            	led_off_red();
+     if (lptmr0_irq_counter !=0){
 
-    	            	lptmr0_irq_counter=0;
+    	 boton1=boton1leerestado();
+    	 boton2=boton2leerestado();
 
-
-    	        }
-
-       //i++ ;
-        //printf("i:%u\r\n",i);
-    	boton1=boton1leerestado();
-    	boton2=boton2leerestado();
-    	if (boton1 && !boton1_presionado){
-    		boton1_presionado=1;
-    		adc_sensor_de_luz=sensorDeLuzObtenerDatosADC();
-    		printf("sensor de luz en unidades  lux :%.4f\r\n",adc_sensor_de_luz);
-    	}
-
-    	if (!boton1){
-    		boton1_presionado=0;
-    	}
-    	if (boton2){
-    	    boton2_presionado=1;
-    	    adc_sensor_de_temperatura=SensorDetemperaturaObtenerDatosADC();
-
-    	}
+    	 if (boton1 && !boton1_presionado){
+    		 boton1_presionado=1;
+    		 printf("BOTON 1 PRESIONADO...\r\n");
+    	 }
+    	 if (!boton1){
+    		 boton1_presionado=0;
+    	 }
 
 
 
-    		if (!boton2){
+    	 if (boton2 && !boton2_presionado){
+    		 boton2_presionado=1;
+    		 printf("BOTON 2 PRESIONADO...\r\n");
+    	 }
+    	 if (!boton2){
+    		 boton2_presionado=0;
+    	 }
 
-    			boton2_presionado=0;
-
-    		}
-
-    }
+     } //fin if lptmr0_irq_counter
 
 
-    }
-   return 0 ;
+    }//fin while  /*****************************************/
+
+  return 0;
 }
